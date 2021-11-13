eee7a4fde01270ad36457fc9af2f0e5d7927699f
Tom <35646257+tom1919@users.noreply.github.com>
2021-11-13 09:56:35 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,15 +6,15 @@ with open("README.md", "r", encoding="utf-8") as fh:
 setup(
   name = 'thelogger',       
   packages = ['thelogger'],  
-  version = 'v0.1.0',      
+  version = 'v0.1.1',      
   license='Apache 2.0',       
-  description = 'super easy to use logger object',  
+  description = 'super easy to use package for logging code',  
   long_description = long_description,
   long_description_content_type = 'text/markdown',
   author = 'Tom1919',                   
   author_email = 'py.notify1@gmail.com',      
   url = 'https://github.com/tom1919/TheLogger,  
-  download_url = 'https://github.com/tom1919/TheLogger/archive/refs/tags/v0.1.0.tar.gz',   
+  download_url = 'https://github.com/tom1919/TheLogger/archive/refs/tags/v0.1.1.tar.gz',   
   keywords = ['log', 'logging', 'logger'],  
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
