5febc9c4d477ad921e2510e7bfe2fbb3c3523f15
Tom <35646257+tom1919@users.noreply.github.com>
2021-11-24 02:32:39 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -15,7 +15,8 @@ setup(
   author_email = 'py.notify1@gmail.com',      
   url = 'https://github.com/tom1919/TheLogger',  
   download_url = 'https://github.com/tom1919/TheLogger/archive/refs/tags/v0.2.0.tar.gz',   
-  keywords = ['log', 'logging', 'logger'],  
+  keywords = ['log', 'logging', 'logger', 'email', 'timimg', 'notification'],  
+  install_requires = ['pandas','tabulate'], 
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
     'Intended Audience :: Developers',     
