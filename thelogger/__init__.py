0f1baeb56ae2f041240f208cb76e32450ae915f8
Tom <35646257+tom1919@users.noreply.github.com>
2021-11-24 02:23:47 -0500

    added import to notify

--- thelogger/__init__.py
+++ thelogger/__init__.py
@@ -2,31 +2,10 @@
 """
 Created on Sat Oct 23 04:36:47 2019
 
-Easy to use logger object for creating code logs. Just import it and it's set up, 
-ready to go.
-
-from thelogger import lg
-
-# log messages
-lg.info('Hello World')
-lg.warning('warning message')
-lg.error('error message')
-
-# start logging messages to a file
-lg.reset(file = './demo_log.txt')
-
-# shorthand convenience methods for logging messages
-lg.i('this is an info message')
-lg.w('this is a warning message')
-
-# remove log handlers
-lg.close()
-lg.i('nothing is logged because there are no log handlers')
-
-# add default log handlers
-lg.reset()
+Easy logging, timing and email notifications of code execution.
 
 @author: tommy
 """
 
 from .logger import logger, log, lg
+from .notify import notify
