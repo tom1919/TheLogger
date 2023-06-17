16dc48e2df5742f5e75c8ea0eeaa1d0497fd76ed
tom1919 <tommywlang@gmail.com>
2023-06-17 00:30:03 -0400

    added context manager timer

--- thelogger/dirty_timer.py
+++ thelogger/dirty_timer.py
@@ -1,12 +1,10 @@
-# -*- coding: utf-8 -*-
 """
-Created on Sat Oct 24 05:22:37 2019
-
-@author: tommy
+Quick and dirty utilties for timing code.
 """
 
 import os
 import time
+import contextlib
 from datetime import timedelta
 
 def beg():
@@ -16,6 +14,28 @@ def beg():
 def end():
     end_tm = time.perf_counter()
     beg_tm = float(os.environ['the_logger_time'])
+    sec_delta = end_tm - beg_tm
+    sec_delta = round(sec_delta, 3)
+    exec_tm = str(timedelta(seconds = sec_delta)).rstrip('0')
+    print(f"Elapsed Time: {exec_tm}")
+    
+@contextlib.contextmanager 
+def timer():
+    """
+    Context manager that provides timing of code execution.
+
+    Example
+    -------
+    >>> import thelogger as tl
+    >>> import time 
+    >>> with timer():
+    ...     time.sleep(1)
+    Elapsed Time: 0:00:00.999
+
+    """
+    beg_tm = time.perf_counter()
+    yield 
+    end_tm = time.perf_counter()
     sec_delta = end_tm - beg_tm
     sec_delta = round(sec_delta, 3)
     exec_tm = str(timedelta(seconds = sec_delta)).rstrip('0')
